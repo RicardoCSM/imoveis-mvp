9b162623ce894ae1ff4683834dfb4c9858b81cd8
Ricardo <ricardocosta1601@gmail.com>
2024-01-26 16:11:59 -0300

    Removing Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
 import { ThemeProvider } from "@material-tailwind/react";
-import NavbarMenu from "./components/navigation/NavbarMenu";
 import Map from "./components/Map";
 
 export default function Home() {
   return (
     <ThemeProvider>
-      <div className="flex flex-col justify-center items-center w-full gap-6">
-        <div className="w-full">
-          <NavbarMenu />
-        </div>
+      <div className="flex flex-col justify-center items-center w-full h-screen gap-6">
         <div className="flex justify-center w-4/5">
             <Map/>
         </div>
